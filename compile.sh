d7d12a4c4ec93396fa9a69df6ce66ba447564e8a
zzc880808 <1253383589@qq.com>
2013-12-04 15:02:41 +0800

    1、增加了grid和col的响应式布局能力。2、修改了测试用例。3、对一些细节进行微调。

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "x" ]; then
-	./node_modules/less/bin/lessc -x ./less/main.less > ./css/main.css
+	./node_modules/less/bin/lessc -x ./less/powerGrid.less > ./css/powerGrid.css
 else
-	./node_modules/less/bin/lessc ./less/main.less > ./css/main.css
+	./node_modules/less/bin/lessc ./less/powerGrid.less > ./css/powerGrid.css
 fi
---
+++ js/powerGrid.js
@@ -0,0 +1,6 @@
+(function($){})(jQuery);
+
+$(function($){
+	alert($(window).width());
+});
+
